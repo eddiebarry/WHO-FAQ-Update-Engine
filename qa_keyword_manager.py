9a6c42cb7889b2f5246e44b4f9adec0b75d0db92
eddiebarry <edgarmonis@gmail.com>
2020-10-16 12:01:01 +0000

    Changing API spec to accomodate front end

--- qa_keyword_manager.py
+++ qa_keyword_manager.py
@@ -7,9 +7,25 @@ class QAKeywordManager:
         if self.index.getIndexDir() != folder_id_path:
             self.index.update_store_dir(folder_id_path) 
 
+        question_array = self.transform_question_array(question_array)
         # store the jsons
         self.index.indexJsonArray(question_array)
-        self.index.print_all_contents()
+        # self.index.print_all_contents()
 
         # update the search engine to use the new data
         self.search_engine.update(self.index.getIndexDir())
+
+    def transform_question_array(self, question_array):
+        for qa_pair in question_array:
+            keywords = qa_pair['keywords']
+            for category_keywords in keywords:
+                keyword_string = ""
+                assert len(category_keywords.keys())==1
+                
+                for x in category_keywords:
+                    keyword_string += " ".join(category_keywords[x])
+                    if keyword_string != " ":
+                        qa_pair[x] = keyword_string
+            qa_pair.pop('keywords',None)
+
+        return question_array
